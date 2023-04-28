80aab99cbfa7e8a7b96a04572ad16e9628351e34
fritzrehde <fritzrehde.work@gmail.com>
2023-04-28 22:04:06 +0200

    Added solar radiation energy factor

---
+++ energy-factors/sun_radiation.py
@@ -0,0 +1,40 @@
+import numpy as np
+from pyproj import Transformer
+import math
+
+# Calculate Sun Radiation for a given coordinate as annual sum of global radiation (kWh/m^2)
+# DWD (Deutscher wetterdienst) Climate dataset
+
+# TODO: maybe add year parameter, as dataset contains yearly data
+
+def get_solar_radiation(latitude, longitude):
+    # Define coordinate systems
+    from_crs = "EPSG:4326"  # WGS 84
+    to_crs = "EPSG:31467"  # Gauss Krüger Zone 3
+
+    # Create transformer object
+    transformer = Transformer.from_crs(from_crs, to_crs)
+
+    # Convert latitude and longitude to Gauss Krüger coordinates
+    h, r = transformer.transform(latitude, longitude)
+
+    # Information extracted from the dataset header
+    XLLCORNER = 3280500
+    YLLCORNER = 5237500
+    NROWS = 866
+    CELLSIZE = 1000
+    NODATA_VALUE = -999
+
+    # Load data as 2d array
+    data = np.loadtxt("data/radiation/grids_germany_annual_radiation_global_2022.asc", skiprows=28)
+    data[data == -999] = np.nan
+
+    y, x = math.floor((r - XLLCORNER) / CELLSIZE), NROWS - math.ceil((h - YLLCORNER) / CELLSIZE)
+    radiance = data[x, y]
+
+    return radiance
+
+# Example: Coordinates of TU Munich
+latitude, longitude = 48.1496636, 11.5656715
+radiance = get_solar_radiation(latitude, longitude)
+print(radiance)
