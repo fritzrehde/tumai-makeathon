53ff6b28493f2ea7e27ce0518c9ae1bb31d829c4
paul <paul.pucknus@tum.de>
2023-04-28 22:33:07 +0200

    Changes

--- get_buildings.py
+++ get_buildings.py
@@ -1,19 +1,35 @@
 from pyrosm import OSM
 import pandas as pd
 
-# osm = OSM("bremen.osm.pbf")
-print('loading data')
-# osm = OSM("bremen-buildings-only.osm.pbf")
-osm = OSM("bremen.osm.pbf")
 
-print('get buildings')
-buildings = osm.get_buildings()
-df = pd.DataFrame(buildings)
+def get_roofshape(dict):
+    if 'roof:shape' in dict.keys():
+        return dict['roof:shape']
+    else:
+        return ("")
 
-drop_list = ['addr:country', 'addr:full', 'addr:housename', 'email',
-       'name', 'opening_hours', 'operator', 'phone', 'ref', 'url', 'website', 'internet_access', 'wikipedia']
 
-df = df.drop(drop_list, axis=1)
-print(df.columns)
-print(df.loc[100, :].values.tolist())
-print(df.head(100).to_csv("test.csv"))
+def get_df():
+    # osm = OSM("bremen.osm.pbf")
+    print('loading data')
+    # osm = OSM("bremen-buildings-only.osm.pbf")
+    osm = OSM('data/buildings/bremen-buildings-only.osm.pbf')
+
+    print('get buildings')
+    buildings = osm.get_buildings()
+    df = pd.DataFrame(buildings)
+
+    drop_list = ['addr:country', 'addr:full', 'addr:housename', 'email',
+                 'name', 'opening_hours', 'operator', 'phone', 'ref', 'url', 'website', 'internet_access', 'wikipedia']
+
+    df = df.drop(drop_list, axis=1)
+    print(df.columns)
+    print(df.loc[100, :].values.tolist())
+    print(df.head(100).to_csv("test.csv"))
+
+    # x = get_roofshape({"roof:shape":"flat","type":"supermarket"})
+    # print(x)
+
+
+if __name__ == '__main__':
+    get_df()
