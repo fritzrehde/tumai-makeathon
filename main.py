0557af3ad99031fbfd27b8a660b79940ab9519d5
Margarita <margarita.shibarshina@tum.de>
2023-04-29 16:38:17 +0200

    added zip code

--- main.py
+++ main.py
@@ -10,35 +10,39 @@ st.title("Solar Opposites")
 street_num = st.text_input("Enter your address")
 zip_code = st.text_input("Enter your zip code")
 
-# Combine street number and zip code to form the complete address
-address = f"{street_num}, Germany, {zip_code}"
+# Create a button to trigger the execution of the code
+button = st.button("Calculate potential energy harvested per year")
 
-# Initialize the geolocator
-geolocator = Nominatim(user_agent="my-app")
+if button:
+    # Combine street number and zip code to form the complete address
+    address = f"{street_num}, Germany, {zip_code}"
 
-# Use the geolocator to get the latitude and longitude of the address
-if address:
-    location = geolocator.geocode(address)
-    if location:
-        latitude, longitude = location.latitude, location.longitude
-        st.write("Latitude:", latitude)
-        st.write("Longitude:", longitude)
+    # Initialize the geolocator
+    geolocator = Nominatim(user_agent="my-app")
 
-        # Check if location is in Germany
-        if not(47.3 <= latitude <= 55.1 and 5.9 <= longitude <= 15.2):
-            st.write("Address is outside of Germany")
-            st.stop()
+    # Use the geolocator to get the latitude and longitude of the address
+    if address:
+        location = geolocator.geocode(address)
+        if location:
+            latitude, longitude = location.latitude, location.longitude
+            st.write("Latitude:", latitude)
+            st.write("Longitude:", longitude)
 
-        # Calculate potential energy harvested per year
-        potential_energy = get_sun_radiation(latitude, longitude) # Use correct function!!
-        
-        # Create a DataFrame with the latitude and longitude information
-        data = pd.DataFrame({"LATITUDE": [latitude], "LONGITUDE": [longitude]})
+            # Check if location is in Germany
+            if not(47.3 <= latitude <= 55.1 and 5.9 <= longitude <= 15.2):
+                st.write("Address is outside of Germany")
+                st.stop()
 
-        # Display map with marker on address
-        st.map(data)
-        
-        st.write("Your house's total potential energy harvested per year (in kWh/m^2), if you were to install solar panels:", potential_energy)
+            # Calculate potential energy harvested per year
+            potential_energy = get_sun_radiation(latitude, longitude) # Use correct function!!
+            
+            # Create a DataFrame with the latitude and longitude information
+            data = pd.DataFrame({"LATITUDE": [latitude], "LONGITUDE": [longitude]})
 
-    else:
-        st.write("Address not found")
+            # Display map with marker on address
+            st.map(data)
+
+            st.write("Your house's total potential energy harvested per year (in kWh/m^2), if you were to install solar panels:", potential_energy)
+
+        else:
+            st.write("Address not found")
