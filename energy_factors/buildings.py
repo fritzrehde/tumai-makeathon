a69dc9eeacbbd185a0fb6122a4a7e9e02d294bb1
paul <paul.pucknus@tum.de>
2023-04-30 00:15:08 +0200

    added new file

--- energy_factors/buildings.py
+++ energy_factors/buildings.py
@@ -13,10 +13,21 @@ from .roof_location import get_rooflocation_longitude
 from .roof_shape import get_roofshape
 from .building_type import get_buildingtype
 
+from azureml.core import Workspace, Datastore, Dataset
+
+# create a FileDataset pointing to files in 'animals' folder and its subfolders recursively
+# datastore_paths = [(datastore, 'animals')]
+# animal_ds = Dataset.File.from_files(path=datastore_paths)
+
+# create a FileDataset from image and label files behind public web urls
+web_paths = ['https://makeathon0835510051.blob.core.windows.net/azureml-blobstore-4f800bf3-3089-4757-9d89-d2a8a7c7b1b6/UI/2023-04-29_191451_UTC/germany-latest.osm.pbf']
+# mnist_ds = Dataset.File.from_files(path=web_paths)
+
 def get_df():
     # import .pbf buildings as df
     print('Debug: loading data from OSM')
-    osm = OSM('azureml://subscriptions/7972acb1-114d-41ac-b5c5-91d74b796b31/resourcegroups/paul.pucknus-rg/workspaces/makeathon/datastores/workspaceblobstore/paths/UI/2023-04-29_191451_UTC/germany-latest.osm.pbf')
+    osm = OSM(web_paths)
+    # osm = OSM('azureml://subscriptions/7972acb1-114d-41ac-b5c5-91d74b796b31/resourcegroups/paul.pucknus-rg/workspaces/makeathon/datastores/workspaceblobstore/paths/UI/2023-04-29_191451_UTC/germany-latest.osm.pbf')
     # osm = OSM('data/buildings/bremen.osm.pbf')
 
     print('Debug: extract buildings from OSM')
