6f6dec6d365b879996aaa97d1c6e6bef907ed088
fritzrehde <fritzrehde.work@gmail.com>
2023-04-29 23:00:06 +0200

    Fixed api calls

--- database.py
+++ database.py
@@ -1,15 +1,15 @@
 import pandas as pd
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from tinydb import TinyDB, Query, where
 
 from energy_factors.buildings import get_df
 
 # Create a TinyDB database and a table
-db = TinyDB('database/tinydb.json')
-table = db.table('buildings')
+# db = TinyDB('database/tinydb.json')
+# table = db.table('buildings')
 
 df = get_df();
-table.insert(df.to_dict())
+# table.insert(df.to_dict())
 
 # Convert the DataFrame to a dictionary and insert it into the table
 print("Debug: started loading dataframe into tinydb")
@@ -36,7 +36,7 @@ def get_data():
     if not record.empty:
         return jsonify(record.to_dict('records')[0])
     else:
-        return jsonify({'error': 'Record not found'})
+        abort(404, description="Record not found")
 
 if __name__ == '__main__':
     app.run(debug=True)
