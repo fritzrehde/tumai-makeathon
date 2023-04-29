f341a8e26be0b5dd2d14ebc7278ea7819513ff1e
fritzrehde <fritzrehde.work@gmail.com>
2023-04-29 19:34:11 +0200

    Added first impl of api + db

--- generate_csv_data.py
+++ generate_csv_data.py
@@ -4,7 +4,9 @@ def run():
     df = get_df()
 
     # Export data to csv file
-    print(df.to_csv("generated_data/test_bremen.csv"))
+    print("Debug: started writing data-frame to csv file")
+    df.to_csv("generated_data/test_bremen.csv")
+    print("Debug: finished writing data-frame to csv file")
 
 if __name__ == '__main__':
     run()
