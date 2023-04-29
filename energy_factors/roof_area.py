1ecb20d0d743aaff40187611ac0c8dfa182c112a
tyou <48173772+tyou1@users.noreply.github.com>
2023-04-29 23:44:36 +0200

    Update roof_area.py

--- energy_factors/roof_area.py
+++ energy_factors/roof_area.py
@@ -2,24 +2,29 @@ from pyrosm import OSM
 import pandas as pd
 import pyproj
 import shapely
-import shapely.wkt
+from shapely import wkt
+from pyproj import Geod
 
 from shapely.ops import transform
 
 # Define the source and destination coordinate systems
-in_proj = pyproj.CRS('EPSG:4326')  # WGS 84
-out_proj = pyproj.CRS('EPSG:3857')  # Web Mercator
-transformer = pyproj.Transformer.from_proj(in_proj, out_proj)
+# in_proj = pyproj.CRS('EPSG:4326')  # WGS 84
+# out_proj = pyproj.CRS('EPSG:3857')  # Web Mercator
+# transformer = pyproj.Transformer.from_proj(in_proj, out_proj)
 
 def get_roofarea(polygon):
     # input polygon is passed directly in the WKT format
 
-    # Convert the polygon string to a Shapely Polygon object
-    # polygon = shapely.wkt.loads(polygon)
-    projected_polygon = shapely.ops.transform(transformer.transform, polygon)
+    # specify a name ellipsoid
+    geod = Geod(ellps="WGS84")
+    area = abs(geod.geometry_area_perimeter(polygon)[0])
 
-    # Calculate the area of the reprojected polygon in square meters
-    area_m2 = projected_polygon.area
+    # # Convert the polygon string to a Shapely Polygon object
+    # # polygon = shapely.wkt.loads(polygon)
+    # projected_polygon = shapely.ops.transform(transformer.transform, polygon)
 
-    return area_m2
+    # # Calculate the area of the reprojected polygon in square meters
+    # area_m2 = projected_polygon.area
+
+    return area
 
